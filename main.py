b0ccabf4ace5794b11357e61915a47911f00ca46
James Philbrick <63430438+jamesphilbrick@users.noreply.github.com>
2023-11-08 15:32:21 +0000

    Add files via upload
    Simplified source of TLE

--- main.py
+++ main.py
@@ -1,69 +1,95 @@
 # AUTHOR: James Philbrick
-# Initially created 15/06/2020
+# Initially created 15/06/2020, updated 08/11/2023
 
-# NOTES:
-# Can add functionality to get names of ISS crew from https://www.nasa.gov/mission_pages/station/expeditions/index.html
-
-# It should be noted that the Vector Time (GMT) displayed represents the most recent TLE string posted by NASA and
-# not the date at which it was sourced.
-
-# If running on LINUX:
-# might be worth looking at https://stackoverflow.com/questions/21322948/beautifulsoup-wont-recognize-lxml
-
-import ephem
-import math
+import urllib.request
 import time
+import math
 from datetime import datetime
-from bs4 import BeautifulSoup
-import requests 
+import ephem
 
-OBS_LON = 0.5406     # observer latitude
-OBS_LAT = 53.2307    # observer longitude
-OBS_ELEVATION = 75   # elevation in m
-UPDATE_DELAY = 60    # delay between each data scrape in seconds
-TIMEOUT_DELAY = 10   # url access timeout delay in seconds
+SATELLITE_NAME = "ISS (ZARYA)"
+TLE_SOURCE_URL = "https://celestrak.org/NORAD/elements/gp.php?GROUP=stations&FORMAT=tle"
+TLE_SOURCE_INTERVAL_HRS = 12
+TLE_SOURCE_INTERVAL_SEC = float(60 * 60 * TLE_SOURCE_INTERVAL_HRS)
+STORED_TLE = ("1 25544U 98067A   23312.38093591  .00013945  00000+0  24951-3 0  9994",
+              "2 25544  51.6428 334.3064 0001085 100.8606  30.1667 15.50269558424151")
+OBS_LON =  0.5406
+OBS_LAT = 53.2307
+OBS_ELEVATION = 75
 
-def tle_source():
-    # NASA has changed their site - this function will need to be updated to allow for automatic collection of the most recent TLE data available.
-    # For now, data is hard-coded at the expense of accuracy.
-    tle_1 = '1 25544U 98067A   22220.55122723  .00004662  00000-0  88788-4 0  9996'
-    tle_2 = '2 25544  51.6445  77.1862 0005231  97.2571 342.7527 15.50269549353300'
-    date = '08/08/2022'
-    motion = 'motion'
+def log(msg):
+    print("[{}] {}".format(time.ctime(time.time()), msg))
 
-    return tle_1, tle_2, date, motion
+def get_tle_url_status() -> str:
+    req = urllib.request.Request(url=TLE_SOURCE_URL, headers={"User-Agent': 'Mozilla/5.0"})
+    return urllib.request.urlopen(req).getcode()
 
-def calc_alt_az(tle_1, tle_2):
-    # calculate altitude and azimuth
+def get_tle() -> tuple:
+    req = urllib.request.Request(url=TLE_SOURCE_URL, headers={"User-Agent': 'Mozilla/5.0"})
+    _ = urllib.request.urlopen(req)
+    url_byte_array = _.read()
+    _.close()
+    url_content = url_byte_array.decode("utf8")
+    url_content = url_content.splitlines(False)
+    for idx, line in enumerate(url_content): 
+        url_content[idx] = line.rstrip(" ")
+    sat_idx = url_content.index(SATELLITE_NAME)
+    tle_1 = url_content[sat_idx + 1]
+    tle_2 = url_content[sat_idx + 2]
+
+    return (tle_1, tle_2)
+
+def varify_tle(tle: tuple) -> bool: # Function not currently used. 
+    if (len(tle[0]) == 69) and (len(tle[1]) == 69):
+        return True
+    else: 
+        return False
+
+
+def calc_alt_az(tle: tuple) -> tuple:
     observer.date = datetime.utcnow()
-    iss_loc = ephem.readtle('ISS', tle_1, tle_2)
+    iss_loc = ephem.readtle(SATELLITE_NAME, tle[0], tle[1])
     iss_loc.compute(observer)
     alt = (iss_loc.alt * 180/math.pi)
     az = (iss_loc.az * 180/math.pi)
 
-    return alt, az
+    return (alt, az)
 
 def main():
-    # source data and then get altitude and azimuth
     try:
-        tle_1, tle_2, date, motion = tle_source()
+        url_status = get_tle_url_status()
+        log("Status = {}".format(url_status))
     except:
-        print('ERROR: unable to source TLE from spaceflight.nasa.gov \n Perhaps there has been a format change on the source page.')
-    alt, az = calc_alt_az(tle_1, tle_2)
+        log("Warning: Status exception when contacting {}".format(TLE_SOURCE_URL))
 
-    # display information
-    print('Last updated: ', datetime.utcnow())
-    print('Elevation and Azimuth (DEG): {} {}'.format(alt, az))
-    print('{} \n {} \n'.format(date.strip(), motion.strip()))
+    log("Obtaining TLE from celestrak.org...")
+    try: 
+        tle = get_tle()
+        log("TLE successfully obtained for {}".format(SATELLITE_NAME))
+        log("Line 1 = {}".format(tle[0]))
+        log("Line 2 = {}".format(tle[1]))
+    except:
+        log("Warning: Unable to source or parse TLE from {}".format(TLE_SOURCE_URL))
+        try: 
+            tle
+            log("Warning: TLE not updated.")
+            log(str(tle))
+        except: 
+            tle = STORED_TLE
+            log("Warning: Using default TLE values: {}. Calculated position likely to be highly inaccurate.".format(STORED_TLE))
+    
+    alt_az = calc_alt_az(tle)
+    log("Elevation & Azumith = {} & {} respectively (DEG).".format(alt_az[0], alt_az[1]))
+    
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     # difine observer
     observer = ephem.Observer()
     observer.lon = OBS_LON
     observer.lat = OBS_LAT
-    observer.elevation = 75
+    observer.elevation = OBS_ELEVATION
+
 
     while True:
         main()
-        time.sleep(UPDATE_DELAY)
+        time.sleep(TLE_SOURCE_INTERVAL_SEC - time.time() % TLE_SOURCE_INTERVAL_SEC)
